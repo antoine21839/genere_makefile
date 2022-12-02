3af510a3454c4558d30644e393ea367f3406ec9d
Antoine B <antoine21839@gmail.com>
2022-12-02 23:00:34 +0100

    Update genere_makefile.c

--- genere_makefile.c
+++ genere_makefile.c
@@ -85,7 +85,7 @@ int ecrire_ldflags_h(FILE *out, const char *nom_fichier_h){
             fprintf(out, "-lm ");
         else if(strcmp(mot_lu, "<ncurses.h>") == 0)
             fprintf(out, "-lncurses ");
-        else if(strcmp(mot_lu, "\"MLV/all.h\"") == 0)
+        else if(strcmp(mot_lu, "<MLV/all.h>") == 0)
             fprintf(out, "-lMLV ");
         else if(strcmp(mot_lu, "<readline\\readline.h>") == 0)
             fprintf(out, "-lreadline ");
